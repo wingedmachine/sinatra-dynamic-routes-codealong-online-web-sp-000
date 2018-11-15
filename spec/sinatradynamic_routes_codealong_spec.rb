ac4ccd8763d230dfbc8054c5931c7dd7b15e9638
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-15 21:21:52 +0000

    automatically backed up by learn

--- spec/sinatradynamic_routes_codealong_spec.rb
+++ spec/sinatradynamic_routes_codealong_spec.rb
@@ -21,6 +21,7 @@ describe 'app.rb' do
   describe 'GET /multiply/:num1/:num2' do
     it 'sends a 200 status code' do
       get '/multiply/5/5'
+      binding.pry
       expect(last_response.status).to eq(200)
     end
 
