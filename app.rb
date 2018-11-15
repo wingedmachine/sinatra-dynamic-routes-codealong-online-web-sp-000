cd1e0ff2a510a254b87848dd994faf640ae9af74
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-15 21:15:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     @user_name = params[:name]
     "Goodbye, #{@user_name}!"
   end
-  
+
   get "/multiply/:num1/:num2" do
     @num1 = params[:num1]
     @num2 = params[:num2]
