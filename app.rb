e9bf367dfe11012558dcfd6ec87c4cb0ce9d0129
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-15 21:26:13 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,9 +20,9 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
+    binding.pry
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_1
-    binding.pry
     (@num1 * @num2).to_s
   end
 end
